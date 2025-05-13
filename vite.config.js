c5f9e19a720a0a61a8a0141d107c6f223f1955d6
Vu Quoc Dung <vuquocdung@192.168.2.156>
2025-05-13 17:08:41 +0700

    init

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "./src/index.html"),
+        about: path.resolve(__dirname, "./src/about.html"),
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});
